ab5d97a895bb855d9f0ef032144627e555be8dce
Broch Stilley <brochington@gmail.com>
2022-08-08 17:25:08 -0700

    A little cleaning up of meta data text.

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Parse plain English language to known structured data">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
